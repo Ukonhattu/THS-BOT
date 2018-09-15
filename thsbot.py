e2196d03858d5abd242ab652b35a11a8a9ef090c
Daniel Koch <daniel.koch@helsinki.fi>
2018-09-15 21:29:51 +0300

    giphy integaaripo bugfix

--- thsbot.py
+++ thsbot.py
@@ -96,7 +96,8 @@ async def prompt():
 async def gif(tag : str):
     try:
         api_response = api_instance.gifs_random_get(api_key, tag=tag)
-        reponse = api_response.json()    
+        with open(api_response) as ar:
+            response = json.load(ar)    
         await bot.say(response['data']['image_url'])
     except ApiException as e:
         print("Exception when calling DefaultApi->gifs_random_get: %s\n" % e)
