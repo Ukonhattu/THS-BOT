5cd60806f324b604ba713abd995b8eff68a0423c
Daniel Koch <daniel.koch@helsinki.fi>
2018-09-15 18:02:23 +0300

    korjauskomentoon

--- thsbot.py
+++ thsbot.py
@@ -19,7 +19,7 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    channel = client.default_channel
+    channel = client.get_channel(490212509962338307)
     await client.send_message(channel, "Olen palannut entistä parempana! (Minut on päivitetty)")
 
 @bot.command()
