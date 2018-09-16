7c319f7b5be9e546a3559b299f33e568d40533ae
Daniel Koch <daniel.koch@helsinki.fi>
2018-09-16 18:30:22 +0300

    yritys poistaa url näkymästä random gifeissä

--- thsbot.py
+++ thsbot.py
@@ -99,8 +99,9 @@ async def gif(rtag : str):
             await bot.say("haulla %s ei löyty gifiä" % rtag)
             return
         embed = discord.Embed()
-        embed.set_image(url=gif)  
-        await bot.say(embed=embed)
+        embed.set_image(url=gif)
+        content = "Random gif tägillä: " + rtag  
+        await bot.say(content=content,embed=embed)
     except ApiException as e:
         print("Exception when calling DefaultApi->gifs_random_get: %s\n" % e)
 
