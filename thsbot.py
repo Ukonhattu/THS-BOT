3b74a377d491d5d1fb745eefb613a82d805d7082
Daniel Koch <daniel.koch@helsinki.fi>
2018-09-20 19:01:18 +0300

    wa debug

--- thsbot.py
+++ thsbot.py
@@ -129,7 +129,7 @@ async def komennot():
 @bot.command()
 async def wa(params):
     res = wa_client.query(params)
-    await bot.say(next(res.results).text)
+    await bot.say(next(res.results))
 
 @bot.command()
 async def translate(text, source='tunnista', target='fi'):
